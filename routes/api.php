5c1a3633bb93e3a51c7122beaeee88f38ab88482
Александр Тимохин <alexx1088@gmail.com>
2025-01-13 14:06:09 +0500

    add Swagger documentation

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware(['validate.api_key'])->group(function () {
 
     Route::get('organization/{organization_id}', [OrganizationController::class, 'organizationById']);
 
-    Route::get('activities/{activity_id}/organizations', [ActivityController::class, 'organizationsByActivityTree']);
+    Route::get('activities/{activity_id}/organizations_list', [ActivityController::class, 'organizationsByActivityTree']);
 
     Route::get('organizations/search', [OrganizationController::class, 'companySearch']);
 
